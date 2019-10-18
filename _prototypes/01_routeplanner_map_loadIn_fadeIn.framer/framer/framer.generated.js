47955d95e45b8ef3f971c9c07da98da311d86775
Michel van Heest <michelvanheest@icloud.com>
2019-10-18 08:22:42 +0200

    Working on the draw, not fixed yet

--- _prototypes/01_routeplanner_map_loadIn_fadeIn.framer/framer/framer.generated.js
+++ _prototypes/01_routeplanner_map_loadIn_fadeIn.framer/framer/framer.generated.js
@@ -7,7 +7,7 @@ if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":"fit","se
 }
 if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-macbook-pro","contentScale":1,"hideBezel":true,"orientation":0};
 }
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"01_routeplanner_map_loadIn_draw.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"01_routeplanner_map_loadIn_fadeIn.framer"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();
