c186b64c9b0f2b9b51997a079a0758bc3045a8af
Michel van Heest <michelvanheest@icloud.com>
2020-01-16 14:59:12 +0100

    Img are fading in

--- js/animate.js
+++ js/animate.js
@@ -82,7 +82,26 @@ $("#caseVideoLink").click(function() {
     }, 800);
 });
 
-
+// FADEIN ON LOAD
 $(document).ready(function () {
     $('div.hidden').fadeIn(1200).removeClass('hidden');
+});
+
+
+
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.fadeIn').each( function(i){
+            // var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_object = $(this).position().top + 150;
+            // var bottom_of_object = $(this).position().top;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},1200);
+            }
+        });
+    });
 });
