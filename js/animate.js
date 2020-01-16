466b4c573fbf70ce43c04ee7b4e6ad3139138de2
Michel van Heest <michelvanheest@icloud.com>
2020-01-16 22:38:20 +0100

    ff kortere fade

--- js/animate.js
+++ js/animate.js
@@ -100,7 +100,7 @@ $(document).ready(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},1200);
+                $(this).animate({'opacity':'1'},800);
             }
         });
     });
